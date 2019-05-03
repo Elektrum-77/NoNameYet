573e80ab100aa134a80d64e303fedb986656e639
BABILLON Brian <brian.babillon@gmail.com>
2019-05-03 20:31:28 +0200

    because it's required

--- Prototypes/Entity/Entity.lua
+++ Prototypes/Entity/Entity.lua
@@ -3,22 +3,37 @@ local Entity =
   Furnace =
   {
     RecipeType = "Smelting",
+    Energy = {Type = "Burner", ConsuptionMultiplier = 2.5},
     Width = 2,
     Height = 2,
-    MaxInput = 1,
-    MaxOutput = 1,
+    Input = {Max = 1},
+    Output = {Max = 1},
     Texture = love.graphics.newImage("Textures/Furnace.png"),
+    
+    Init = function(self)
+      self.Input.Inventory = {}
+      self.Output.Inventory = {}
+      self.Energy.Inventory = {}
+    end,
   },
   OreCrackingStation =
   {
     RecipeType = "OreCracking",
+    Energy = {Type = "Burner", ConsuptionMultiplier = 2.5},
     Width = 2,
     Height = 2,
-    MaxInput = 2,
-    MaxOutput = 4,
+    Input = {Max = 2},
+    Output = {Max = 4},
+    Texture = love.graphics.newImage("Textures/Furnace.png"),
+    
+    Init = function(self)
+      self.Input.Inventory = {}
+      self.Output.Inventory = {}
+      self.Energy.Inventory = {}
+    end,
   },
 }
 
 for k,v in pairs(Entity) do
-  game.Prototypes.Entity[k] = v
+  Game.Prototypes.Entity[k] = v
 end
--- main.lua
+++ main.lua
@@ -65,12 +65,12 @@ function love.load()
     },
     Player = 
     {
-      X = 0, Y = 0,
+      X = 0, Y = 0, MapX = 0, MapY = 0, RX = 0, RY = 0,
       
       IsInventoryOpen = false,
       Inventory = {OffsetY = 0}, MouseItem = nil,
-      
-      Speed = 16, MiningSpeed = 25,
+      BlueprintRotation = {MaxX = 1, MaxY = 1, MinX = 0, MinY = 0},
+      Speed = 8, MiningSpeed = 25,
       ToolbarCount = 1,
       
       Textures = 
@@ -82,22 +82,28 @@ function love.load()
       --Angle = {0, math.pi/2, math.pi/4,math.pi, nil, 3*math.pi/4, math.pi/2, 3*math.pi/2, 7*math.pi/4, nil, 0, 5*math.pi/4, 3*math.pi/2, math.pi, nil},
       
       Init = function(self)
+        
         self.NextX = self.X
         self.NextY = self.Y
+        
         local Item = {Content = "O2", ContentCount = 100}
         setmetatable(Item, {__index = Game.Prototypes.Item.Bottle})
         AddItem(self.Inventory, Item)
+        
         for __ = 1,5 do
           local Item = {Content = "Empty"}
           setmetatable(Item, {__index = Game.Prototypes.Item.Bottle})
           AddItem(self.Inventory, Item)
         end
+        
         Item = {Count = 1}
         setmetatable(Item, {__index = Game.Prototypes.Item.Furnace})
         AddItem(self.Inventory, Item)
+        
         Item = {Count = 1}
         setmetatable(Item, {__index = Game.Prototypes.Item.OreCrackingStation})
         AddItem(self.Inventory, Item)
+        
       end,
       
       Draw = function(self)
@@ -177,7 +183,7 @@ function love.load()
           if self.MouseItem then
             if self.Inventory[k] then
               local Item = self.MouseItem
-              print("test1")
+              --print("test1")
               setmetatable(Item, getmetatable(self.MouseItem))
               
               self.MouseItem = self.Inventory[k]
@@ -186,7 +192,7 @@ function love.load()
               AddItem(self.Inventory, Item)
               --setmetatable(self.Inventory[k], getmetatable(Item))
             else
-              print("test2")
+              --print("test2")
               AddItem(self.Inventory, self.MouseItem)
               --setmetatable(self.Inventory[#self.Inventory], getmetatable(self.MouseItem))
               
@@ -194,7 +200,7 @@ function love.load()
             end
           else
             if self.Inventory[k] then
-              print("test3")
+              --print("test3")
               self.MouseItem = self.Inventory[k]
               setmetatable(self.MouseItem, getmetatable(self.Inventory[k]))
               table.remove(self.Inventory, k)
@@ -208,6 +214,27 @@ function love.load()
         if Key == "e" then
           self.IsInventoryOpen = not self.IsInventoryOpen
           --print(self.IsInventoryOpen)
+        end
+        if Key == "r" then
+          local BR = self.BlueprintRotation -- this is did in that way to be "read friendly" / understable and readable
+          if BR.MaxX == 1 and BR.MaxY == 1 then
+            BR.MinX = -1
+            BR.MaxX = 0
+          elseif BR.MaxY == 1 then
+            BR.MinY = -1
+            BR.MaxY = 0
+          elseif BR.Max == 0 then
+            BR.MinX = 0
+            BR.MaxX = 1
+          else
+            BR.MinY = 0
+            BR.MaxY = 1
+          end
+          self.BlueprintRotation = BR
+        end
+        if Key == "a" then
+          AddItem(self.Inventory, self.MouseItem)
+          self.MouseItem = nil
         end
       end,
       
@@ -230,18 +257,14 @@ function love.load()
         end
         
         local c, l = math.floor(((self.NextX+20)%41)-20), math.floor(((self.NextY+20)%41)-20)
-        local MapX, MapY = math.floor(((self.X+20)%41)-20), math.floor(((self.Y+20)%41)-20)
+        self.MapX, self.MapY = math.floor(((self.X+20)%41)-20), math.floor(((self.Y+20)%41)-20)
         local Rc, Rl = math.floor((self.NextX+20)/41), math.floor((self.NextY+20)/41)
-        local Rx, Ry = math.floor((self.X+20)/41), math.floor((self.Y+20)/41)
-        if Ry >= Game.Maps.MinY and Ry <= Game.Maps.MaxY and Rc >= Game.Maps.MinX and Rc <= Game.Maps.MaxX then
-          if not Game.Maps.Region[Ry][Rc].Grid[MapY][c].Entity or not Game.Maps.Region[Ry][Rc].Grid[MapY][c].Entity.Collide then
-            self.X = self.NextX
-          end
+        self.RX, self.RY = math.floor((self.X+20)/41), math.floor((self.Y+20)/41)
+        if not Game.Maps.Region[self.RY][Rc].Grid[self.MapY][c].Entity or not Game.Maps.Region[self.RY][Rc].Grid[self.MapY][c].Entity.Collide then
+          self.X = self.NextX
         end
-        if Rl >= Game.Maps.MinY and Rl <= Game.Maps.MaxY and Rx >= Game.Maps.MinX and Rx <= Game.Maps.MaxX then
-          if not Game.Maps.Region[Rl][Rx].Grid[l][MapX].Entity or not Game.Maps.Region[Rl][Rx].Grid[l][MapX].Entity.Collide then
-            self.Y = self.NextY
-          end
+        if not Game.Maps.Region[Rl][self.RX].Grid[l][self.MapX].Entity or not Game.Maps.Region[Rl][self.RX].Grid[l][self.MapX].Entity.Collide then
+          self.Y = self.NextY
         end
         --print(self.NextX, self.NextY, c, l, Rc, Rl)
         
@@ -249,6 +272,49 @@ function love.load()
         if Game.Mouse.Pressed[1] then
           local Case = Game.Maps.Region[Game.Mouse.RegionY][Game.Mouse.RegionX].Grid[Game.Mouse.MapY][Game.Mouse.MapX]
           
+          if not self.IsInventoryOpen and self.MouseItem and self.MouseItem.Entity then
+            if not Case.Entity then
+              
+              local Obstacle
+              
+              for i = 1, 2 do
+                for Y = Game.Mouse.MapY + self.BlueprintRotation.MinY, Game.Mouse.MapY + self.BlueprintRotation.MaxY do
+                  for X = Game.Mouse.MapX + self.BlueprintRotation.MinX, Game.Mouse.MapX + self.BlueprintRotation.MaxX do
+                    
+                    local RY, RX = Game.Mouse.RegionY, Game.Mouse.RegionX
+                    if Y > 20 then
+                      Y = Y - 41
+                      RY = RY + 1
+                    elseif Y < -20 then
+                      Y = Y + 41
+                      RY = RY - 1
+                    end 
+                    if X > 20 then
+                      X = X - 41
+                      RX = RX + 1
+                    elseif X < -20 then
+                      X = X + 41
+                      RX = RX - 1
+                    end
+                    print(X, Y, RX, RY)
+                    if Game.Maps.Region[RY][RX].Grid[Y][X].Entity then Obstacle = true end
+                    
+                    if i == 2 and not Obstacle then
+                      Game.Maps.Region[RY][RX].Grid[Y][X].Entity = {EntityID = #Game.Entity + 1, Collide = true}
+                    end
+                    
+                  end
+                end
+              end
+              
+              if not Obstacle then
+                table.insert(Game.Entity, {})
+                setmetatable(Game.Entity[#Game.Entity], {__index = Game.Prototypes.Entity[self.MouseItem.Entity]})
+                Game.Entity[#Game.Entity]:Init()
+              end
+              
+            end
+          end
           
         end
         if Game.Mouse.Pressed[2] then
@@ -286,6 +352,13 @@ function love.load()
       end,
     },
     
+    Entity = 
+    {
+      Update = function(self, dt)
+        
+      end,
+    },
+    
     Maps = 
     {
       MinX = -2,
@@ -307,9 +380,28 @@ function love.load()
         self.Region[0][0].Grid[-1][-1].Entity = {Collide = true}
       end,
       
+      Update = function(self, dt)
+        self.MinY, self.MinX, self.MaxY, self.MaxX = Game.Player.RY, Game.Player.RX, Game.Player.RY, Game.Player.RX
+        if Game.Player.MapY <= -10 then self.MinY = self.MinY - 1 elseif Game.Player.MapY >= 10 then self.MaxY = self.MaxY + 1 end
+        if Game.Player.MapX <= -4 then self.MinX = self.MinX - 1 elseif Game.Player.MapX >= 4 then self.MaxX = self.MaxX + 1 end
+      end,
+      
       Draw = function(self)
-        for l = self.MinY,self.MaxY do
-          for c = self.MinX,self.MaxX do
+        for l = self.MinY, self.MaxY do
+          for c = self.MinX, self.MaxX do
+            if not self.Region[l] then
+              self.Region[l] = {}
+              if l > self.MaxY then self.MaxY = l elseif l < self.MinY then self.MinY = l end
+              --print(l, self.MaxY, self.MinY, "test1")
+            end
+            if not self.Region[l][c] then
+              if c > self.MaxX then self.MaxX = c elseif c < self.MinX then self.MinX = c end
+              --print(c, self.MaxX, self.MinX, "test2")
+              self.Region[l][c] = {}
+              setmetatable(self.Region[l][c], {__index = Game.Prototypes.Region})
+              self.Region[l][c]:Init()
+              self.Region[l][c]:GenerateOrePatch({Game.Prototypes.Ore.List[math.ceil(math.random()*#Game.Prototypes.Ore.List)], Game.Prototypes.Ore.List[math.ceil(math.random()*#Game.Prototypes.Ore.List)]})
+            end
             self.Region[l][c]:Draw(Game.Player.X*(-32) + ScreenWidth/2 + c*41*32, Game.Player.Y*(-32)+ ScreenHeight/2 + l*41*32)
           end
         end
@@ -321,6 +413,7 @@ function love.load()
       Init = function(self)
         require("Prototypes/Ore/Ore")
         require("Prototypes/Item/Item")
+        require("Prototypes/Entity/Entity")
         for __,v in pairs(Game.Prototypes) do
           if type(v) == "table" then
             for ID, Object in pairs(v) do
@@ -331,6 +424,11 @@ function love.load()
       end,
       
       Item = 
+      {
+        List = {},
+      },
+      
+      Entity = 
       {
         List = {},
       },
@@ -421,6 +519,8 @@ function love.load()
               end
             end
           end
+          Color.Set(Color.Yellow)
+          love.graphics.rectangle("fill", x-20*32-1, y-20*32-1, 3, 3)
         end,
         
         Init = function(self)
@@ -447,7 +547,7 @@ function love.load()
             local r1 = math.floor(math.random() * (7-3) + 3)
             local r2 = math.floor(math.random() * (7-3) + 3)
             
-            print(x1, y1, x2, y2)
+            --print(x1, y1, x2, y2)
             
             for l = -self.Height, self.Height do
               for c = -self.Width, self.Width do
